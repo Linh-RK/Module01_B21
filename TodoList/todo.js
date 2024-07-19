6be280a68b36e67f5e2c28d27fd8f7e3bfb5acb3
Linh Linh <pttlinh101193@gmail.com>
2024-07-19 07:51:01 +0700

    Add files via upload

---
+++ TodoList/todo.js
@@ -0,0 +1,48 @@
+const dbTodo = [
+  { id: 1, todo: "Học ngữ pháp J", status: true },
+  { id: 2, todo: "Hoc từ vưng Tettei", status: false },
+  { id: 3, todo: "Xem 5 video", status: true },
+  { id: 4, todo: "Đi bộ", status: false },
+];
+const todoList = document.getElementById("listTodo");
+function render() {
+  let stringHTML = "";
+  for (let idx = 0; idx < dbTodo.length; idx++) {
+    stringHTML += `
+    <li
+    id = "" 
+    class = "${dbTodo[idx].status}" 
+    onclick = changeStatus(${dbTodo[idx].id})
+    >
+    <p>${dbTodo[idx].todo}</p>
+    <button onclick = del(${dbTodo[idx].id}) text-decoration: none;>
+    Xoa
+    </button>
+    </li>
+    `;
+  }
+  todoList.innerHTML = stringHTML;
+}
+render();
+
+function changeStatus(idchange) {
+  let findIndex = dbTodo.findIndex((el) => el.id === idchange);
+  dbTodo[findIndex].status = !dbTodo[findIndex].status;
+  render();
+}
+function del(idchange) {
+  let findIndex = dbTodo.findIndex((el) => el.id === idchange);
+  dbTodo.splice(dbTodo[findIndex], 1);
+  render();
+}
+const add = document.getElementById("add");
+add.addEventListener("click", function () {
+  let newTodo = {
+    id: Math.random(),
+    todo: input.value,
+    status: false,
+  };
+  dbTodo.push(newTodo);
+  render();
+  input.value = "";
+});
